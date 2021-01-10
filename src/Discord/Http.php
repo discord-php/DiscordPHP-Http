a74929c138e51518c464d43635f57173e07e796e
David Cole <david.cole1340@gmail.com>
2021-01-10 18:40:36 +1300

    Fixed issue where global rate-limit will be returned in place of non-global

--- src/Discord/Http.php
+++ src/Discord/Http.php
@@ -289,7 +289,7 @@ class Http
                     });
                 }
 
-                $deferred->reject($this->rateLimit);
+                $deferred->reject($rateLimit->isGlobal() ? $this->rateLimit : $rateLimit);
             }
             // Bad Gateway
             // Cloudflare SSL Handshake error
