bed3dccb2854ae026eb091ca2b276195be60ef91
David Cole <david.cole1340@gmail.com>
2021-07-03 11:38:44 +1200

    Updated php-cs-fixer to ^3, moved LICENSE

--- src/Discord/Endpoint.php
+++ src/Discord/Endpoint.php
@@ -3,10 +3,10 @@
 /*
  * This file is a part of the DiscordPHP-Http project.
  *
- * Copyright (c) 2021 David Cole <david.cole1340@gmail.com>
+ * Copyright (c) 2021-present David Cole <david.cole1340@gmail.com>
  *
- * This source file is subject to the MIT license that is
- * bundled with this source code in the LICENSE.md file.
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE file.
  */
 
 namespace Discord\Http;
@@ -14,162 +14,162 @@ namespace Discord\Http;
 class Endpoint
 {
     // GET
-    const GATEWAY = 'gateway';
+    public const GATEWAY = 'gateway';
     // GET
-    const GATEWAY_BOT = self::GATEWAY.'/bot';
+    public const GATEWAY_BOT = self::GATEWAY.'/bot';
 
     // GET, POST
-    const GLOBAL_APPLICATION_COMMANDS = 'applications/:application_id/commands';
+    public const GLOBAL_APPLICATION_COMMANDS = 'applications/:application_id/commands';
     // GET, PATCH, DELETE
-    const GLOBAL_APPLICATION_COMMAND = 'application/:application_id/commands/:command_id';
+    public const GLOBAL_APPLICATION_COMMAND = 'application/:application_id/commands/:command_id';
     // GET, POST
-    const GUILD_APPLICATION_COMMANDS = 'application/:application_id/guilds/:guild_id/commands';
+    public const GUILD_APPLICATION_COMMANDS = 'application/:application_id/guilds/:guild_id/commands';
     // GET, PATCH, DELETE
-    const GUILD_APPLICATION_COMMAND = 'application/:application_id/guilds/:guild_id/commands/:command_id';
+    public const GUILD_APPLICATION_COMMAND = 'application/:application_id/guilds/:guild_id/commands/:command_id';
     // POST
-    const INTERACTION_RESPONSE = 'interactions/:interaction_id/:interaction_token/callback';
+    public const INTERACTION_RESPONSE = 'interactions/:interaction_id/:interaction_token/callback';
     // PATCH, DELETE
-    const ORIGINAL_INTERACTION_RESPONSE = 'webhooks/:application_id/:interaction_token/messages/@original';
+    public const ORIGINAL_INTERACTION_RESPONSE = 'webhooks/:application_id/:interaction_token/messages/@original';
     // POST
-    const CREATE_INTERACTION_FOLLOW_UP = 'webhooks/:application_id/:interaction_token';
+    public const CREATE_INTERACTION_FOLLOW_UP = 'webhooks/:application_id/:interaction_token';
     // PATCH, DELETE
-    const INTERACTION_FOLLOW_UP = 'webhooks/:application_id/:interaction_token/messages/:message_id';
+    public const INTERACTION_FOLLOW_UP = 'webhooks/:application_id/:interaction_token/messages/:message_id';
 
     // GET
-    const AUDIT_LOG = 'guilds/:guild_id/audit-logs';
+    public const AUDIT_LOG = 'guilds/:guild_id/audit-logs';
 
     // GET, PATCH, DELETE
-    const CHANNEL = 'channels/:channel_id';
+    public const CHANNEL = 'channels/:channel_id';
     // GET, POST
-    const CHANNEL_MESSAGES = self::CHANNEL.'/messages';
+    public const CHANNEL_MESSAGES = self::CHANNEL.'/messages';
     // GET, PATCH, DELETE
-    const CHANNEL_MESSAGE = self::CHANNEL.'/messages/:message_id';
+    public const CHANNEL_MESSAGE = self::CHANNEL.'/messages/:message_id';
     // POST
-    const CHANNEL_CROSSPOST_MESSAGE = self::CHANNEL.'/messages/:message_id/crosspost';
+    public const CHANNEL_CROSSPOST_MESSAGE = self::CHANNEL.'/messages/:message_id/crosspost';
     // POST
-    const CHANNEL_MESSAGES_BULK_DELETE = self::CHANNEL.'/messages/bulk-delete';
+    public const CHANNEL_MESSAGES_BULK_DELETE = self::CHANNEL.'/messages/bulk-delete';
     // PUT, DELETE
-    const CHANNEL_PERMISSIONS = self::CHANNEL.'/permissions/:overwrite_id';
+    public const CHANNEL_PERMISSIONS = self::CHANNEL.'/permissions/:overwrite_id';
     // GET, POST
-    const CHANNEL_INVITES = self::CHANNEL.'/invites';
+    public const CHANNEL_INVITES = self::CHANNEL.'/invites';
     // POST
-    const CHANNEL_FOLLOW = self::CHANNEL.'/followers';
+    public const CHANNEL_FOLLOW = self::CHANNEL.'/followers';
     // POST
-    const CHANNEL_TYPING = self::CHANNEL.'/typing';
+    public const CHANNEL_TYPING = self::CHANNEL.'/typing';
     // GET
-    const CHANNEL_PINS = self::CHANNEL.'/pins';
+    public const CHANNEL_PINS = self::CHANNEL.'/pins';
     // PUT, DELETE
-    const CHANNEL_PIN = self::CHANNEL.'/pins/:message_id';
+    public const CHANNEL_PIN = self::CHANNEL.'/pins/:message_id';
 
     // GET, DELETE
-    const MESSAGE_REACTION_ALL = self::CHANNEL.'/messages/:message_id/reactions';
+    public const MESSAGE_REACTION_ALL = self::CHANNEL.'/messages/:message_id/reactions';
     // GET, DELETE
-    const MESSAGE_REACTION_EMOJI = self::CHANNEL.'/messages/:message_id/reactions/:emoji';
+    public const MESSAGE_REACTION_EMOJI = self::CHANNEL.'/messages/:message_id/reactions/:emoji';
     // PUT, DELETE
-    const OWN_MESSAGE_REACTION = self::CHANNEL.'/messages/:message_id/reactions/:emoji/@me';
+    public const OWN_MESSAGE_REACTION = self::CHANNEL.'/messages/:message_id/reactions/:emoji/@me';
     // DELETE
-    const USER_MESSAGE_REACTION = self::CHANNEL.'/messages/:message_id/reactions/:emoji/:user_id';
+    public const USER_MESSAGE_REACTION = self::CHANNEL.'/messages/:message_id/reactions/:emoji/:user_id';
 
     // GET, POST
-    const CHANNEL_WEBHOOKS = self::CHANNEL.'/webhooks';
+    public const CHANNEL_WEBHOOKS = self::CHANNEL.'/webhooks';
 
     // POST
-    const GUILDS = 'guilds';
+    public const GUILDS = 'guilds';
     // GET, PATCH, DELETE
-    const GUILD = 'guilds/:guild_id';
+    public const GUILD = 'guilds/:guild_id';
     // GET, POST, PATCH
-    const GUILD_CHANNELS = self::GUILD.'/channels';
+    public const GUILD_CHANNELS = self::GUILD.'/channels';
 
     // GET
-    const GUILD_MEMBERS = self::GUILD.'/members';
+    public const GUILD_MEMBERS = self::GUILD.'/members';
     // GET, PATCH, PUT, DELETE
-    const GUILD_MEMBER = self::GUILD.'/members/:user_id';
+    public const GUILD_MEMBER = self::GUILD.'/members/:user_id';
     // PATCH
-    const GUILD_MEMBER_SELF_NICK = self::GUILD.'/members/@me/nick';
+    public const GUILD_MEMBER_SELF_NICK = self::GUILD.'/members/@me/nick';
     // PUT, DELETE
-    const GUILD_MEMBER_ROLE = self::GUILD.'/members/:user_id/roles/:role_id';
+    public const GUILD_MEMBER_ROLE = self::GUILD.'/members/:user_id/roles/:role_id';
 
     // GET
-    const GUILD_BANS = self::GUILD.'/bans';
+    public const GUILD_BANS = self::GUILD.'/bans';
     // GET, PUT, DELETE
-    const GUILD_BAN = self::GUILD.'/bans/:user_id';
+    public const GUILD_BAN = self::GUILD.'/bans/:user_id';
 
     // GET, PATCH
-    const GUILD_ROLES = self::GUILD.'/roles';
+    public const GUILD_ROLES = self::GUILD.'/roles';
     // GET, POST, PATCH, DELETE
-    const GUILD_ROLE = self::GUILD.'/roles/:role_id';
+    public const GUILD_ROLE = self::GUILD.'/roles/:role_id';
 
     // GET, POST
-    const GUILD_INVITES = self::GUILD.'/invites';
+    public const GUILD_INVITES = self::GUILD.'/invites';
 
     // GET, POST
-    const GUILD_INTEGRATIONS = self::GUILD.'/integrations';
+    public const GUILD_INTEGRATIONS = self::GUILD.'/integrations';
     // PATCH, DELETE
-    const GUILD_INTEGRATION = self::GUILD.'/integrations/:integration_id';
+    public const GUILD_INTEGRATION = self::GUILD.'/integrations/:integration_id';
     // POST
-    const GUILD_INTEGRATION_SYNC = self::GUILD.'/integrations/:integration_id/sync';
+    public const GUILD_INTEGRATION_SYNC = self::GUILD.'/integrations/:integration_id/sync';
 
     // GET, POST
-    const GUILD_EMOJIS = self::GUILD.'/emojis';
+    public const GUILD_EMOJIS = self::GUILD.'/emojis';
     // GET, PATCH, DELETE
-    const GUILD_EMOJI = self::GUILD.'/emojis/:emoji_id';
+    public const GUILD_EMOJI = self::GUILD.'/emojis/:emoji_id';
 
     // GET
-    const GUILD_PREVIEW = self::GUILD.'/preview';
+    public const GUILD_PREVIEW = self::GUILD.'/preview';
     // GET, POST
-    const GUILD_PRUNE = self::GUILD.'/prune';
+    public const GUILD_PRUNE = self::GUILD.'/prune';
     // GET
-    const GUILD_REGIONS = self::GUILD.'/regions';
+    public const GUILD_REGIONS = self::GUILD.'/regions';
     // GET, PATCH
-    const GUILD_WIDGET_SETTINGS = self::GUILD.'/widget';
+    public const GUILD_WIDGET_SETTINGS = self::GUILD.'/widget';
     // GET
-    const GUILD_WIDGET = self::GUILD.'/widget.json';
+    public const GUILD_WIDGET = self::GUILD.'/widget.json';
     // GET
-    const GUILD_WIDGET_IMAGE = self::GUILD.'/widget.png';
+    public const GUILD_WIDGET_IMAGE = self::GUILD.'/widget.png';
     // GET
-    const GUILD_VANITY_URL = self::GUILD.'/vanity-url';
+    public const GUILD_VANITY_URL = self::GUILD.'/vanity-url';
     // GET, PATCH
-    const GUILD_MEMBERSHIP_SCREENING = self::GUILD.'/member-verification';
+    public const GUILD_MEMBERSHIP_SCREENING = self::GUILD.'/member-verification';
     // GET
-    const GUILD_WEBHOOKS = self::GUILD.'/webhooks';
+    public const GUILD_WEBHOOKS = self::GUILD.'/webhooks';
 
     // GET, DELETE
-    const INVITE = 'invites/:code';
+    public const INVITE = 'invites/:code';
 
     // GET, PATCH
-    const USER_CURRENT = 'users/@me';
+    public const USER_CURRENT = 'users/@me';
     // GET
-    const USER = 'users/:user_id';
+    public const USER = 'users/:user_id';
     // GET
-    const USER_CURRENT_GUILDS = self::USER_CURRENT.'/guilds';
+    public const USER_CURRENT_GUILDS = self::USER_CURRENT.'/guilds';
     // DELETE
-    const USER_CURRENT_GUILD = self::USER_CURRENT.'/guilds/:guild_id';
+    public const USER_CURRENT_GUILD = self::USER_CURRENT.'/guilds/:guild_id';
     // GET, POST
-    const USER_CURRENT_CHANNELS = self::USER_CURRENT.'/channels';
+    public const USER_CURRENT_CHANNELS = self::USER_CURRENT.'/channels';
     // GET
-    const USER_CURRENT_CONNECTIONS = self::USER_CURRENT.'/connections';
+    public const USER_CURRENT_CONNECTIONS = self::USER_CURRENT.'/connections';
     // GET
-    const APPLICATION_CURRENT = 'oauth2/applications/@me';
+    public const APPLICATION_CURRENT = 'oauth2/applications/@me';
 
     // GET, PATCH, DELETE
-    const WEBHOOK = 'webhooks/:webhook_id';
+    public const WEBHOOK = 'webhooks/:webhook_id';
     // GET, PATCH, DELETE
-    const WEBHOOK_TOKEN = 'webhooks/:webhook_id/:webhook_token';
+    public const WEBHOOK_TOKEN = 'webhooks/:webhook_id/:webhook_token';
     // POST
-    const WEBHOOK_EXECUTE = self::WEBHOOK_TOKEN;
+    public const WEBHOOK_EXECUTE = self::WEBHOOK_TOKEN;
     // POST
-    const WEBHOOK_EXECUTE_SLACK = self::WEBHOOK_EXECUTE.'/slack';
+    public const WEBHOOK_EXECUTE_SLACK = self::WEBHOOK_EXECUTE.'/slack';
     // POST
-    const WEBHOOK_EXECUTE_GITHUB = self::WEBHOOK_EXECUTE.'/github';
+    public const WEBHOOK_EXECUTE_GITHUB = self::WEBHOOK_EXECUTE.'/github';
     // PATCH, DELETE
-    const WEBHOOK_MESSAGE = self::WEBHOOK_TOKEN.'/messages/:message_id';
+    public const WEBHOOK_MESSAGE = self::WEBHOOK_TOKEN.'/messages/:message_id';
 
     /**
      * Regex to identify parameters in endpoints.
      *
      * @var string
      */
-    const REGEX = '/:([^\/]*)/';
+    public const REGEX = '/:([^\/]*)/';
 
     /**
      * A list of parameters considered 'major' by Discord.
@@ -177,7 +177,7 @@ class Endpoint
      * @see https://discord.com/developers/docs/topics/rate-limits
      * @var string[]
      */
-    const MAJOR_PARAMETERS = ['channel_id', 'guild_id', 'webhook_id'];
+    public const MAJOR_PARAMETERS = ['channel_id', 'guild_id', 'webhook_id'];
 
     /**
      * The string version of the endpoint, including all parameters.
