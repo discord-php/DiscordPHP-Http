c73d6afb03282c7cf2f4ddb4bf096c9f23fafad1
SQKo <87897282+SQKo@users.noreply.github.com>
2022-05-24 00:41:27 +0700

    Update Endpoint.php

--- src/Discord/Endpoint.php
+++ src/Discord/Endpoint.php
@@ -3,7 +3,7 @@
 /*
  * This file is a part of the DiscordPHP-Http project.
  *
- * Copyright (c) 2022-present David Cole <david.cole1340@gmail.com>
+ * Copyright (c) 2021-present David Cole <david.cole1340@gmail.com>
  *
  * This file is subject to the MIT license that is bundled
  * with this source code in the LICENSE file.
