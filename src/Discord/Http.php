f8453028d2e00ec8eb2c3f58c6a8e89eb6b8d9fe
Valithor Obsidion <valzargaming@gmail.com>
2025-08-21 14:12:54 -0400

    Bump version

--- src/Discord/Http.php
+++ src/Discord/Http.php
@@ -30,7 +30,7 @@ class Http implements HttpInterface
      *
      * @var string
      */
-    public const VERSION = 'v10.4.8';
+    public const VERSION = 'v10.4.9';
 
     /**
      * Current Discord HTTP API version.
