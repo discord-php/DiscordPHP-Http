955bded538fbf505d77866db41a31e885c236ca6
Valithor Obsidion <valzargaming@gmail.com>
2025-09-22 12:13:18 -0400

    LIST_VOICE_REGIONS

--- src/Discord/Endpoint.php
+++ src/Discord/Endpoint.php
@@ -198,6 +198,8 @@ class Endpoint implements EndpointInterface
     public const GUILD_WELCOME_SCREEN = self::GUILD.'/welcome-screen';
     // GET
     public const GUILD_ONBOARDING = self::GUILD.'/onboarding';
+    // GET
+    public const LIST_VOICE_REGIONS = 'voice/regions';
     // GET, PATCH
     public const GUILD_USER_CURRENT_VOICE_STATE = self::GUILD.'/voice-states/@me';
     // GET, PATCH
